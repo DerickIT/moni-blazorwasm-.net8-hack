2f5fbe5583a0a8fbec885d9f8bc7e319bb94e1e4
Derick <charleszhn@hotmail.com>
2023-12-03 19:39:35 +0800

    update main code summary

--- MoniBlazorDotnet8Hack/Pages/Snake.razor.cs
+++ MoniBlazorDotnet8Hack/Pages/Snake.razor.cs
@@ -4,12 +4,24 @@ using Microsoft.JSInterop;
 
 namespace MoniBlazorDotnet8Hack.Pages;
 
+/*
+* Snake.razor.cs
+* Author：@DerickIT
+*/
 public partial class Snake
 {
     #region [SNAKE DEFAULT VALUES]
 
+
+    /// <summary>
+    /// Represents a single cell in the snake game. This could be a part of the snake's body, or an empty cell.
+    /// </summary>
     SnakeCell currentCell;
 
+    /// <summary>
+    /// Represents the Score class.
+    /// This class is used to keep track of the score in the game.
+    /// </summary>
     Score score = new();
 
     // Define the Snake's initial direction
@@ -35,6 +47,11 @@ public partial class Snake
 
     #region [METHODS]
 
+    /// <summary>
+    /// This method is called when the component is initialized.
+    /// It initializes the game and starts it.
+    /// </summary>
+    /// <returns>A Task that represents the asynchronous operation.</returns>
     protected override async Task OnInitializedAsync()
     {
         InitializeGame();
@@ -42,6 +59,10 @@ public partial class Snake
         await StartGame();
     }
 
+
+    /// <summary>
+    /// Initializes the game by setting the initial state of the snake and generating the first food.
+    /// </summary>
     private void InitializeGame()
     {
         // Define the Snake's initial position
@@ -57,6 +78,13 @@ public partial class Snake
         GenerateFood();
     }
 
+
+    /// <summary>
+    /// Starts the game if it hasn't already started.
+    /// This method is responsible for the game loop where it updates the snake's direction,
+    /// checks if the food is found, increments the score, generates new food, and updates the game state.
+    /// </summary>
+    /// <returns>A Task representing the asynchronous operation.</returns>
     private async Task StartGame()
     {
         if (!gameStarted)
